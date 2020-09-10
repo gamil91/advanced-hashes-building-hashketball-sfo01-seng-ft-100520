65a8d0101af96510a2880f572da9eba748d1801e
Alexa Gamil <alexis_gamil99@yahoo.com>
2020-09-10 18:50:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,16 +3,21 @@ require 'pry'
 def game_hash
 { 
   :home {
+    :team_name => " ",
+    :colors => [" ", " "],
+    :players => [
+      :player_name => " "
+      
+      
+                ]
     
-  }
+        },
     
   :away {
       
       
       
-  }
-    
-    
+        }
     
 }
 end
